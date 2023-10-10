720b942030942b606d8329b5857bb74084ac9811
Rokimajo <robinvliegtvandaag@gmail.com>
2023-10-10 19:43:16 +0200

    Application now uses PostgreSQL fully instead of SQLite.

--- ProjectC/CaveroApp/Program.cs
+++ ProjectC/CaveroApp/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+// using postgres for identity database
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
