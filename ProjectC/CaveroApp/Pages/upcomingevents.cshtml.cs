6a1bd70397ca76f0627d8d60140af8079393a504
AryanJadoenath <1061627@hr.nl>
2023-10-15 14:03:22 +0200

    UpcomingEvents files/tabellen zijn aangemaakt en een beetje ingevuld.
    Ik heb als eerste alle files aangemaakt. Daarna heb ik de tabel aangemaakt en deze heb ik alvast gevuld met wat komende events. Het is nog niet scrollable of responsive en ik moet het nog even netjes maken. Vergeet niet de 'Information' icoon/knop.

---
+++ ProjectC/CaveroApp/Pages/upcomingevents.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaveroApp.Areas.Identity.Pages.Account
+{
+    public class UpcomingEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
