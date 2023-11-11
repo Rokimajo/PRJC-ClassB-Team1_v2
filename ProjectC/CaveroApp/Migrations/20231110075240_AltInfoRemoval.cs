f6971c6bef08bf286be24c294743f6acbee01e40
Rokimajo <robinvliegtvandaag@gmail.com>
2023-11-11 19:20:51 +0100

    Added migrations & new DBContext over from events tab. Main is now error free.

---
+++ ProjectC/CaveroApp/Migrations/20231110075240_AltInfoRemoval.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaveroApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AltInfoRemoval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "alt_info",
+                table: "Events");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "alt_info",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
