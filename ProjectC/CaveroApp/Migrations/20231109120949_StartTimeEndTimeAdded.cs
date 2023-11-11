c0a5ef8e378b1ae085c93de739c976afe27c04cd
Rokimajo <robinvliegtvandaag@gmail.com>
2023-11-11 19:20:51 +0100

    Added migrations & new DBContext over from events tab. Main is now error free.

---
+++ ProjectC/CaveroApp/Migrations/20231109120949_StartTimeEndTimeAdded.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaveroApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class StartTimeEndTimeAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "time",
+                table: "Events",
+                newName: "start_time");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "end_time",
+                table: "Events",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "end_time",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "start_time",
+                table: "Events",
+                newName: "time");
+        }
+    }
+}
