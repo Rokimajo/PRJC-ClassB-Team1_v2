08cca7a46db9b82d7b5a9599ecc02f8ba7cc6cab
hello <94355994+SvenGravemaker@users.noreply.github.com>
2023-11-15 18:56:43 +0100

    pagina voor de info

---
+++ ProjectC/CaveroApp/Pages/adminUserInfo.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using CaveroApp.Areas.Identity.Data;
+using CaveroApp.Data;
+
+namespace CaveroApp.Pages;
+
+public class adminUserInfo : PageModel
+{
+    public CaveroAppContext Context { get; }
+    public void OnGet()
+    {
+        
+    }
+    public adminUserInfo(CaveroAppContext context)
+    {
+        Context = context;
+    }
+
+    public List<CaveroAppUser> Users()
+    {
+        return Context.Users.ToList();
+    }
+}
