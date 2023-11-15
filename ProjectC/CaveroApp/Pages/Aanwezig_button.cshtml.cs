50c3639f8487dcbea931dfe90db0a810bf219b4a
hello <94355994+SvenGravemaker@users.noreply.github.com>
2023-11-15 14:12:35 +0100

    aparte functies voor in/uitchecken

--- ProjectC/CaveroApp/Pages/Aanwezig_button.cshtml.cs
+++ ProjectC/CaveroApp/Pages/Aanwezig_button.cshtml.cs
@@ -3,25 +3,13 @@ using System.Security.Claims;
 using CaveroApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
 
 namespace CaveroApp.Pages;
 
 public class Aanwezig_button : PageModel
 {
     private CaveroAppContext Context { get; }
-    public string ButtonText { get; set; }
-    
-
-    public void OnGet()
-    {
-        ButtonText = AvailableToday() ? "Zet jezelf afwezig" : "Zet jezelf aanwezig";
-    }
-
-    public IActionResult OnPostChangeStatus()
-    {
-        ChangeStatus();
-        return Page();
-    }
     public Aanwezig_button(CaveroAppContext context)
     {
         Context = context;
@@ -35,30 +23,34 @@ public class Aanwezig_button : PageModel
             where DateTime.UtcNow.Date.Equals(a.date.Date) select u.Id).ToList();
         return AreAtWork.Contains(userId);
     }
-    public string ChangeStatus()
+    public string SetUnavailable()
     {
         string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        if (AvailableToday())
+        if (userId != null) 
         {
-            CaveroAppContext.Attendance currentUser = (from a in Context.Attendances 
-                where a.user_id == userId && a.date == DateTime.UtcNow.Date select a).First();
-            Context.Attendances.Remove(currentUser);
+            List<CaveroAppContext.Attendance> currentUser = (from a in Context.Attendances 
+                where a.user_id == userId && a.date == DateTime.UtcNow.Date select a).ToList();
+            foreach (CaveroAppContext.Attendance attendance in currentUser)
+            {
+                Context.Attendances.Remove(attendance);
+            }
             Context.SaveChanges();
-            ButtonText = "Zet jezelf aanwezig";
             Console.WriteLine("afwezig gezet");
         }
-        else
-        {
-            if (userId != null)
-            {
-                CaveroAppContext.Attendance newAttendance = new CaveroAppContext.Attendance()
-                    { user_id = userId, date = DateTime.UtcNow.Date };
-                Context.Attendances.Add(newAttendance);
-                Context.SaveChanges();
-                ButtonText = "Zet jezelf afwezig";
-                Console.WriteLine("aanwezig gezet");
-            }
+        return "Attendance changed to unavailable";
+    }
+
+    public string SetAvailable()
+    {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (userId != null) 
+        {   
+        CaveroAppContext.Attendance newAttendance = new CaveroAppContext.Attendance()
+            { user_id = userId, date = DateTime.UtcNow.Date };
+        Context.Attendances.Add(newAttendance);
+        Context.SaveChanges();
+        Console.WriteLine("aanwezig gezet");
         }
-        return "Attendance Changed";
+        return "Attendance changed to available";
     }
 }
--- ProjectC/CaveroApp/Pages/Dashboard.cshtml.cs
+++ ProjectC/CaveroApp/Pages/Dashboard.cshtml.cs
@@ -12,7 +12,6 @@ public class Dashboard : PageModel
     
     public int EmployeeCount { get; set; }
     public int EventCount { get; set; }
-    public string ButtonText { get; set; }
 
     // use dependency injection to get the database context
     public Dashboard(CaveroAppContext context)
@@ -45,46 +44,5 @@ public class Dashboard : PageModel
         // Call this function to populate the fields with how many employees are present today,
         // and how many events are happening this week.
         GetTodayStats();
-        if (AvailableToday())
-        {
-            ButtonText = "Zet jezelf afwezig";
-        }
-        else
-        {
-            ButtonText = "Zet jezelf aanwezig";
-        }
-    }
-    public bool AvailableToday()
-    {
-        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        var AreAtWork = (from a in Context.Attendances 
-            join u in Context.Users on a.user_id equals u.Id
-            where DateTime.UtcNow.Date.Equals(a.date.Date) select u.Id).ToList();
-        return AreAtWork.Contains(userId);
-    }
-
-    public string ChangeStatus()
-    {
-        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        if (AvailableToday())
-        {
-            CaveroAppContext.Attendance currentUser = (from a in Context.Attendances 
-                where a.user_id == userId && a.date == DateTime.UtcNow.Date select a).First();
-            Context.Attendances.Remove(currentUser);
-            Context.SaveChanges();
-            ButtonText = "Zet jezelf aanwezig";
-        }
-        else
-        {
-            if (userId != null)
-            {
-                CaveroAppContext.Attendance newAttendance = new CaveroAppContext.Attendance()
-                    { user_id = userId, date = DateTime.UtcNow.Date };
-                Context.Attendances.Add(newAttendance);
-                Context.SaveChanges();
-                ButtonText = "Zet jezelf afwezig";
-            }
-        }
-        return "Attendance Changed";
     }
 }
