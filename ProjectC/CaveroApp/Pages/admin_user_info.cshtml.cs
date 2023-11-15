79a7eeee92372c7f1acefc085191a9cfae975320
hello <94355994+SvenGravemaker@users.noreply.github.com>
2023-11-15 16:58:42 +0100

    styled button

---
+++ ProjectC/CaveroApp/Pages/admin_user_info.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using CaveroApp.Areas.Identity.Data;
+using CaveroApp.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaveroApp.Pages;
+
+public class admin_user_info : PageModel
+{
+    private CaveroAppContext Context { get; }
+    public admin_user_info(CaveroAppContext context)
+    {
+        Context = context;
+    }
+    public void OnGet()
+    {
+        
+    }
+    
+}
