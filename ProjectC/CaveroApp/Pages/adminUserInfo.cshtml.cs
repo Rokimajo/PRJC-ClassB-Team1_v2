19a81874bca0190af1a6d796340b0206d15cea7b
hello <94355994+SvenGravemaker@users.noreply.github.com>
2023-11-15 22:08:29 +0100

    forms en edit button toegevoegd.
    Als een user wordt gekozen, moet de info nog worden geladen in die forms. aanpassingen moeten worden opgeslagen

--- ProjectC/CaveroApp/Pages/adminUserInfo.cshtml.cs
+++ ProjectC/CaveroApp/Pages/adminUserInfo.cshtml.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using CaveroApp.Areas.Identity.Data;
 using CaveroApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CaveroApp.Pages;
 
 public class adminUserInfo : PageModel
 {
     public CaveroAppContext Context { get; }
+    public CaveroAppUser UserToEdit { get; set; }
     public void OnGet()
     {
         
@@ -19,5 +21,16 @@ public class adminUserInfo : PageModel
     public List<CaveroAppUser> Users()
     {
         return Context.Users.ToList();
+    }
+
+    public CaveroAppUser GetChosenUser(string id)
+    {
+        
+    }
+    public IActionResult OnPostUpdateUser(CaveroAppUser updatedUser)
+    {
+        Context.Users.Update(updatedUser);
+        Context.SaveChanges();
+        return RedirectToPage();
     }
 }
