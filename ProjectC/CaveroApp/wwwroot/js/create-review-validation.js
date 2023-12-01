df60c525cc45ac135689dcb5d274dcd5e1061d68
Cabonari <114401343+Cabonari@users.noreply.github.com>
2023-12-01 12:13:47 +0100

    test

---
+++ ProjectC/CaveroApp/wwwroot/js/create-review-validation.js
@@ -0,0 +1,16 @@
+﻿function validateStars(isEdit) {
+    var stars = document.getElementById('reviewStars').value;
+    if (stars < 1 || stars > 5) {
+        document.getElementById('starsError').innerHTML = 'Kies een aantal sterren tussen 1 en 5.';
+    } else {
+        document.getElementById('starsError').innerHTML = '';
+    }
+}
+function validateFeedback() {
+    var review = document.getElementById('reviewText').value;
+    if (review.length < 1 || review.length > 500) {
+        document.getElementById('reviewError').innerHTML = 'Review moet 1-500 karakters bevatten.';
+    } else {
+        document.getElementById('reviewError').innerHTML = '';
+    }
+}
